e3a086aeaf637afc8bf68d92775f032af1b0eb31
Safwan Morshed <safan41@outlook.com>
2017-08-28 13:47:33 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function setup() {
+  var textInput;
+  textInput = document.getElementById('message');
+  textInput.focus();
+}
+
+window.addEventListener('DOMContentLoaded', setup, false);
+
+window.addEventListener('beforeunload', function(event) {
+  // This example has been updated to improve cross-browser compatibility (as recommended by MDN)
+  var message = 'U CAN NOT LEB';
+  (event || window.event).returnValue = message;
+  return message;
+});
